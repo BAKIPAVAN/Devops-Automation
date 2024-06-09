b46968c877eb2830a0b3d6e54754bf170976f474
BAKIPAVAN <pavanbaki.p@outlook.com>
2024-06-09 15:22:29 +0530

    logger added

--- src/main/java/com/example/demo/BuildDeploymenyApplication.java
+++ src/main/java/com/example/demo/BuildDeploymenyApplication.java
@@ -1,13 +1,17 @@
 package com.example.demo;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class BuildDeploymenyApplication {
+	
+	public static org.slf4j.Logger Logger=LoggerFactory.getLogger(BuildDeploymenyApplication.class);
 
 	public static void main(String[] args) {
 		SpringApplication.run(BuildDeploymenyApplication.class, args);
+		Logger.info("Started....................");
 		System.out.println("Started.......");
 	}
 
